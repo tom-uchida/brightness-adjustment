dd1d08bcf7647d1c555d8a0fd801ddb816fcb166
Tomomasa UCHIDA <tomomasa.is.0930@gmail.com>
2018-09-26 23:12:04 +0900

    update

--- convert_color_tone.py
+++ convert_color_tone.py
@@ -1,5 +1,7 @@
-# Convert Color Tone of Image
-# RGB → HSV → Convert Color Tone → RGB
+# ========================================
+#       Convert Color Tone of Image
+# ========================================
+# BGR → RGB → HSV → Convert Color Tone → RGB
 
 import cv2, matplotlib
 import numpy as np
@@ -8,12 +10,13 @@ plt.style.use('seaborn-white')
 
 
 
-# ------------------------------
-# ----- Placement settings -----
-# ------------------------------
-fig, ax = plt.subplots(2, figsize=(10, 4)) # figsize(width, height)
-ax[0] = plt.subplot2grid((1,2), (0,0))
-ax[1] = plt.subplot2grid((1,2), (0,1))
+# -----------------------------
+# ----- Placement setting -----
+# -----------------------------
+fig, ax = plt.subplots(3, figsize=(8, 6)) # figsize(width, height)
+ax[0] = plt.subplot2grid((2,2), (0,0))
+ax[1] = plt.subplot2grid((2,2), (0,1))
+ax[2] = plt.subplot2grid((2,2), (1,0), colspan=2)
 
 
 
@@ -29,7 +32,9 @@ def read_img(_img_name):
 
   return img_RGB
 
-img_in_RGB = read_img('images/noised_butai_600_340_RL100.jpg')
+#img_in_RGB = read_img('images/noised_butai_600_340_RL10.jpg')
+#img_in_RGB = read_img('images/noised_butai_600_340_RL100.jpg')
+img_in_RGB = read_img('images/Lenna.jpg')
 print("Input image(RGB) : ", img_in_RGB.shape) # （height × width × 色数）
 print("\n")
 
@@ -47,7 +52,7 @@ def show_HSV_values(_hsv_img, _hsv_img_name, _y, _x):
   return
 
 show_HSV_values(img_in_HSV, 
-                "Input image(HSV)", 
+                " Input image(HSV)",
                 int(img_in_HSV.shape[0]*0.5), 
                 int(img_in_HSV.shape[0]*0.5))
 
@@ -56,60 +61,57 @@ show_HSV_values(img_in_HSV,
 # ------------------------------
 # ----- Convert Color Tone -----
 # ------------------------------
-def convert_color_tone(_hsv_img, _hue, _sat, _val):
+def convert_color_tone(_hsv_img, _val_param, _sat_param, _hue_param):
   # -----------------
   # ----- Value -----
   # -----------------
-  #_hsv_img[:, :, 2] = np.add(_hsv_img[:, :, 2], _val)
-  hsv_img_val = _hsv_img[:, :, 2]
-  hsv_img_val = np.add(hsv_img_val, _val)
+  #_hsv_img[:, :, 2] = np.add(_hsv_img[:, :, 2], _val_param)
+  val = _hsv_img[:, :, 2]
+  val = np.add(val, _val_param)
 
   # 0~255の範囲に収める
-  hsv_img_val[hsv_img_val < 0]   = 0
-  hsv_img_val[255 < hsv_img_val] = 255
-
-  # apply
-  _hsv_img[:, :, 2] = hsv_img_val
+  val[val < 0]   = 0
+  val[255 < val] = 255
 
 
 
   # ----------------------
   # ----- Saturation -----
   # ----------------------
-  hsv_img_sat = _hsv_img[:, :, 1]
-  hsv_img_sat = np.divide(hsv_img_sat, _sat)
+  sat = _hsv_img[:, :, 1]
+  sat = np.divide(sat, _sat_param)
 
   # 0~255の範囲に収める
-  hsv_img_sat[hsv_img_sat < 0]   = 0
-  hsv_img_sat[255 < hsv_img_sat] = 255
+  sat[sat < 0]   = 0
+  sat[255 < sat] = 255
 
-  # apply
-  _hsv_img[:, :, 1] = hsv_img_sat
 
 
   # ---------------
   # ----- Hue -----
   # ---------------
-  hsv_img_hue = _hsv_img[:, :, 0]
-  #hsv_img_hue = np.subtract(hsv_img_hue, _hue)
+  hue = _hsv_img[:, :, 0]
 
-  # 0~255の範囲に収める
-  hsv_img_hue[hsv_img_sat < 0]   = 0
-  hsv_img_hue[170 < hsv_img_sat] = 179
-
-  # apply
-  _hsv_img[:, :, 0] = hsv_img_hue
+  # 0°~180°の範囲に収める
+  hue[hue < 0]   = 0
+  hue[179 < hue] = 179
 
 
 
-  return
+  # Apply conversion
+  img_HSV = np.empty((_hsv_img.shape[0], _hsv_img.shape[1], 3), dtype=np.uint8)
+  img_HSV[:, :, 0] = hue
+  img_HSV[:, :, 1] = sat
+  img_HSV[:, :, 2] = val
+
+  return img_HSV
 
 # ===============================================
 #      It is required to adjust parameters.
 # ===============================================
-convert_color_tone(img_in_HSV, 0, 1.5, 10)
+img_out_HSV = convert_color_tone(img_in_HSV, 25, 2, 0)
 
-show_HSV_values(img_in_HSV, 
+show_HSV_values(img_out_HSV, 
                 "Output image(HSV)", 
                 int(img_in_HSV.shape[0]*0.5), 
                 int(img_in_HSV.shape[0]*0.5))
@@ -119,7 +121,7 @@ show_HSV_values(img_in_HSV,
 # ------------------------------
 # ----- Convert HSV to RGB -----
 # ------------------------------
-img_out_RGB = cv2.cvtColor(img_in_HSV, cv2.COLOR_HSV2RGB)
+img_out_RGB = cv2.cvtColor(img_out_HSV, cv2.COLOR_HSV2RGB)
 
 def show_RGB_values(_rgb_img, _rgb_img_name, _y, _x):
   print(_rgb_img_name,"[",_y,",",_x,"] → (R G B) = (",
@@ -158,6 +160,20 @@ show_img(1, img_out_RGB, "Output Image")
 # ----------------------------------------------------
 # ----- Compare Histograms of Input/Output image -----
 # ----------------------------------------------------
+# Hue histogram
+hist_img_in_hue,  bins_in_hue  = np.histogram(img_in_HSV[:,:,0].ravel(),  180, [0, 180])
+hist_img_out_hue, bins_out_hue = np.histogram(img_out_HSV[:,:,0].ravel(), 180, [0, 180])
+
+# plot
+ax[2].set_title("Hue histogram")
+# ax[2].hist(img_in_HSV[:,:,0].ravel(),  bins=50, alpha=0.4, label=" Input Image")
+# ax[2].hist(img_out_HSV[:,:,0].ravel(), bins=50, alpha=0.4, label=" Output Image")
+ax[2].plot(hist_img_in_hue,  label="Input Image")
+ax[2].plot(hist_img_out_hue, label="Output Image")
+ax[2].set_xlabel("Hue value")
+ax[2].set_ylabel("Frequency")
+ax[2].legend()
+plt.show()
 
 
 
@@ -165,7 +181,3 @@ show_img(1, img_out_RGB, "Output Image")
 # ----- Save image -----
 # ----------------------
 # cv2.imwrite('images/out.png', img_out_RGB)
-
-
-
-plt.show()
