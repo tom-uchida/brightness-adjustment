87375f2760009385429636ee9aebc78c0414129f
Tomomasa UCHIDA <tomomasa.is.0930@gmail.com>
2018-11-04 23:41:47 +0900

    update

--- visualize_pixel_value_distribution.py
+++ visualize_pixel_value_distribution.py
@@ -6,6 +6,9 @@
 
 import cv2, matplotlib
 import numpy as np
+import subprocess
+import sys
+args = sys.argv
 import matplotlib.pyplot as plt
 plt.style.use('seaborn-white')
 
@@ -24,11 +27,15 @@ ax[3] = plt.subplot2grid((2,2), (1,1))
 
 
 
-# -------------------------------
-# ----- Select input image  -----
-# -------------------------------
-image_name = 'images/2018-10-29/DATA/20160724_RL100.bmp'
-param = 2.46
+# ---------------------------------
+# ----- Set initial parameter -----
+# ---------------------------------
+print("\n===== Argument info. =====")
+image_name  = args[1]
+param       = float(args[2])
+print("input_image_data\n>",image_name,"(args[1])")
+print("\np\n>",param,"(args[2])")
+# image_name = 'images/2018-10-29/DATA/20160724_RL100.bmp'
 
 
 
@@ -45,7 +52,7 @@ def read_img(_img_name):
   return img_RGB
 
 img_in_RGB = read_img(image_name)
-print("Input image(RGB) : ", img_in_RGB.shape) # （height × width × 色数）
+print("\nInput image(RGB)\n>", img_in_RGB.shape) # （height × width × 色数）
 
 
 
@@ -75,9 +82,9 @@ def change_color_tone(_rgb_img, _param):
 # ===============================================
 img_out_RGB = change_color_tone(img_in_RGB, param)
 print("\nOutput Image (RGB)")
-print('R Max:',np.max(img_out_RGB[:, :, 0]),' Min:',np.min(img_out_RGB[:, :, 0]))
-print('G Max:',np.max(img_out_RGB[:, :, 1]),' Min:',np.min(img_out_RGB[:, :, 1]))
-print('B Max:',np.max(img_out_RGB[:, :, 2]),' Min:',np.min(img_out_RGB[:, :, 2]))
+print('> R Max:',np.max(img_out_RGB[:, :, 0]),' Min:',np.min(img_out_RGB[:, :, 0]))
+print('> G Max:',np.max(img_out_RGB[:, :, 1]),' Min:',np.min(img_out_RGB[:, :, 1]))
+print('> B Max:',np.max(img_out_RGB[:, :, 2]),' Min:',np.min(img_out_RGB[:, :, 2]))
 print("\n")
 
 
@@ -96,11 +103,11 @@ img_out_Gray = cv2.cvtColor(img_out_RGB, cv2.COLOR_RGB2GRAY)
 def get_data_of_pixel_value(_img_gray, _img_name):
   print("===== Statistical Data of", _img_name, "(Gray) =====")
   print("Num of pixel values (== 255) :", np.sum(_img_gray == 255))
-  print("Num of pixel values (== 0)   :", np.sum(_img_gray == 0) )
-  print("\nMax :", np.max(_img_gray))
-  print("Min :", np.min(_img_gray))
-  print("\nAverage :", np.mean(_img_gray))
-  print("Median  :", np.median(_img_gray))
+  #print("Num of pixel values (== 0)   :", np.sum(_img_gray == 0) )
+  print("Max  :", np.max(_img_gray))
+  print("Min  :", np.min(_img_gray))
+  print("Mean :", _img_gray[_img_gray>0].mean())
+  #print("Median  :", _img_gray[_img_gray>0].median())
   print("\n")
   return 
 
@@ -133,8 +140,8 @@ def show_img(_i, _img, _img_name):
 # --------------------------------------
 # ----- Display Input/Output Image -----
 # --------------------------------------
-show_img(0, img_in_RGB,  "Input Image")
-show_img(1, img_out_RGB, "Output Image")
+show_img(0, img_in_RGB,  "Input image")
+show_img(1, img_out_RGB, "Improved image(p="+str(param)+")")
 # show_img(2, img_in_Gray,  "Input Image(Gray)")
 # show_img(3, img_out_Gray, "Output Image(Gray)")
 
@@ -146,7 +153,7 @@ show_img(1, img_out_RGB, "Output Image")
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 # Input Image
-ax[2].set_title("Input Image")
+ax[2].set_title("Input image")
 img3 = ax[2].imshow(img_in_Gray, clim=[0,255], cmap='viridis')
 ax[2].axis("image")
 divider = make_axes_locatable(ax[2])
@@ -156,7 +163,7 @@ plt.colorbar(img3, cax=ax2_cb)
 ax[2].axis('off')
 
 # Output Image
-ax[3].set_title("Output Image")
+ax[3].set_title("Improved image(p="+str(param)+")")
 img4 = ax[3].imshow(img_out_Gray, clim=[0,255], cmap='viridis')
 ax[3].axis("image")
 divider = make_axes_locatable(ax[3])
@@ -175,13 +182,24 @@ ax[3].axis('off')
 # plt.colorbar()
 # plt.clim(0, 255)
 
-
-plt.show()
-
+#plt.show()
 
 
-# ----------------------
-# ----- Save image -----
-# ----------------------
-# img_out_BGR = cv2.cvtColor(img_out_RGB, cv2.COLOR_RGB2BGR)
-# cv2.imwrite('images/out.jpg', img_out_BGR)
+
+# -----------------------
+# ----- Save figure -----
+# -----------------------
+fig_name = "images/visualized_"+str(param)+".png"
+plt.savefig(fig_name)
+
+
+
+# -------------------------
+# ----- Exec. command -----
+# -------------------------
+preview_command = ['code', fig_name]
+try:
+	res = subprocess.check_call(preview_command)
+
+except:
+	print("ERROR")
