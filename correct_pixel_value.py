f1c158432fcea0433fe26f9e3cb27c3945be4ae6
Tomomasa UCHIDA <tomomasa.is.0930@gmail.com>
2019-02-25 23:58:37 +0900

    Update

--- correct_pixel_value.py
+++ correct_pixel_value.py
@@ -1,7 +1,7 @@
 ######################################
 #   @file   correct_pixel_value.py
 #   @author Tomomasa Uchida
-#   @date   2019/02/08
+#   @date   2019/02/25
 ######################################
 
 import numpy as np
@@ -25,27 +25,30 @@ plt.rcParams["mathtext.fontset"] = "stix"
 plt.rcParams["mathtext.rm"] = "Times New Roman"
 
 # Message
-print("===================================")
-print("     Correct Pixel Value")
+print("====================================")
+print("     Re-tuning Image Brightness")
 print("      author : Tomomasa Uchida")
-print("      date   : 2019/02/08")
-print("===================================")
+print("      date   : 2019/02/25")
+print("====================================")
 
 # Check arguments
 args = sys.argv
-if len(args) != 3:
-    print("\nUSAGE        : $ python correct_pixel_value.py [input_image_data] [input_image_data(LR=1)]")
-    print("Example      : $ python correct_pixel_value.py [input_image.bmp] [input_image_LR1.bmp]]")
+if len(args) != 4:
+    print("\nUSAGE        : $ python correct_pixel_value.py [input_image_data] [input_image_data(LR=1)] [back_ground_color]")
+    print("Example      : $ python correct_pixel_value.py [input_image.bmp] [input_image_LR1.bmp] [Black:0 or White:255]")
     #raise Exception
     sys.exit()
 
 # Set initial parameter
 p_init      = 1.0
 p_interval  = 0.01
+ratio_of_reference_section = 0.01 # 1(%)
 print("\nInput image data (args[1])       :", args[1])
 print("Input image data(LR=1) (args[2]) :", args[2])
-print("p_init                           :", p_init)
-print("p_interval                       :", p_interval)
+# print("Background color (args[3])       :", args[3])
+# print("p_init                           :", p_init)
+# print("p_interval                       :", p_interval)
+# print("Ratio of reference section       :", ratio_of_reference_section*100, "(%)")
 
 
 
@@ -58,6 +61,21 @@ def readImage(_img_name):
     img_RGB = cv2.cvtColor(img_BGR, cv2.COLOR_BGR2RGB)
 
     return img_RGB
+
+
+
+def checkBackgroundColor(_bgcolor):
+    if int(_bgcolor) == 0:
+        print("Background color                 : Black(0, 0, 0)")
+        return 0
+
+    elif int(_bgcolor) == 255:
+        print("Background color                 : White(255, 255, 255)")
+        return 255
+
+    else:
+        print("\n** The background color(args[3]) is not defined. (Black:0 or White:255)")
+        sys.exit()
 
 
 
@@ -127,8 +145,8 @@ def comparativeHist(_img_in_rgb_LR1, _img_in_rgb, _img_out_rgb, _ax, _y_max):
 
 
 
-# Create Histogram
-def createHistogram(_img_in_RGB_LR1, _img_in_RGB, _img_corrected_RGB, _standard_pixel_value_LR1):
+# Create Figure
+def createFigure(_img_in_RGB_LR1, _img_in_RGB, _img_corrected_RGB, _standard_pixel_value_LR1):
     # Convert RGB to Grayscale
     img_in_Gray_LR1     = cv2.cvtColor(_img_in_RGB_LR1, cv2.COLOR_RGB2GRAY)
     img_in_Gray         = cv2.cvtColor(_img_in_RGB, cv2.COLOR_RGB2GRAY)
@@ -218,45 +236,67 @@ def preProcess():
     N_all = img_in_RGB.shape[0] * img_in_RGB.shape[1]
     print("N_all                            :", N_all, "(pixels)")
 
-    # Calc number of pixels that pixel value is not 0
-    img_in_Gray     = cv2.cvtColor(img_in_RGB, cv2.COLOR_RGB2GRAY)
-    N_all_nonzero   = np.sum(img_in_Gray > 0)
-    print("N_all_nonzero                    :", N_all_nonzero, "(pixels)")
+    # Convert RGB to Grayscale
+    img_in_Gray             = cv2.cvtColor(img_in_RGB, cv2.COLOR_RGB2GRAY)
 
-    # Calc max pixel value of the input image(LR=1)
-    img_in_Gray_LR1     = cv2.cvtColor(img_in_RGB_LR1, cv2.COLOR_RGB2GRAY)
-    N_all_nonzero_LR1   = np.sum(img_in_Gray_LR1 > 0)
-    max_pixel_value_LR1 = np.max(img_in_Gray_LR1)
-    print("Max pixel value (LR=1)           :", max_pixel_value_LR1, "(pixel value)")
+    # Exclude background color
+    img_in_Gray_non_bgcolor = img_in_Gray[img_in_Gray != bgcolor]
+    
+    # Calc the number of pixels excluding background color
+    N_all_non_bgcolor       = np.sum(img_in_Gray != bgcolor)
+    print("N_all_non_bgcolor                :", N_all_non_bgcolor, "(pixels)")
+
+    # Calc mean pixel value
+    max_pixel_value         = np.max(img_in_Gray_non_bgcolor)
+    print("Max pixel value                  :", max_pixel_value, "(pixel value)")
+
+    # Calc mean pixel value
+    mean_pixel_value        = np.mean(img_in_Gray_non_bgcolor)
+    print("Mean pixel value                 :", round(mean_pixel_value, 1), "(pixel value)")
+
+    return N_all_non_bgcolor
+
+
+
+def preProcess4LR1():
+    # Calc max pixel value of the input image (LR=1)
+    img_in_Gray_LR1         = cv2.cvtColor(img_in_RGB_LR1, cv2.COLOR_RGB2GRAY)
+    N_all_non_bgcolor_LR1   = np.sum(img_in_Gray_LR1 != bgcolor)
+    max_pixel_value_LR1     = img_in_Gray_LR1[img_in_Gray_LR1 != bgcolor].max()
+    print("\nMax pixel value (LR=1)           :", max_pixel_value_LR1, "(pixel value)")
+
+    # Calc mean pixel value (LR=1)
+    mean_pixel_value_LR1    = img_in_Gray_LR1[img_in_Gray_LR1 != bgcolor].mean()
+    print("Mean pixel value (LR=1)          :", round(mean_pixel_value_LR1, 2), "(pixel value)")
 
     # Calc ratio of the max pixel value (LR=1)
     num_max_pixel_value_LR1   = np.sum(img_in_Gray_LR1 == max_pixel_value_LR1)
     print("Num. of max pixel value (LR=1)   :", num_max_pixel_value_LR1, "(pixels)")
-    ratio_max_pixel_value_LR1 = num_max_pixel_value_LR1 / N_all_nonzero_LR1
+    ratio_max_pixel_value_LR1 = num_max_pixel_value_LR1 / N_all_non_bgcolor_LR1
     # ratio_max_pixel_value_LR1 = round(ratio_max_pixel_value, 4)
     ratio_max_pixel_value_LR1 = round(ratio_max_pixel_value_LR1, 8)
     print("Ratio of max pixel value (LR=1)  :", round(ratio_max_pixel_value_LR1*100, 2), "(%)")
 
     # Calc most frequent pixel value (LR=1)
-    img_in_Gray_nonzero_LR1         = img_in_Gray_LR1[img_in_Gray_LR1 > 0]
-    bincount = np.bincount(img_in_Gray_nonzero_LR1)
+    img_in_Gray_non_bgcolor_LR1     = img_in_Gray_LR1[img_in_Gray_LR1 != bgcolor]
+    bincount = np.bincount(img_in_Gray_non_bgcolor_LR1)
     most_frequent_pixel_value_LR1   = np.argmax( bincount )
     print("Most frequent pixel value (LR=1) :", most_frequent_pixel_value_LR1, "(pixel value)")
 
-    return N_all_nonzero, N_all_nonzero_LR1, img_in_Gray_LR1, max_pixel_value_LR1, ratio_max_pixel_value_LR1
+    return N_all_non_bgcolor_LR1, img_in_Gray_LR1, max_pixel_value_LR1, ratio_max_pixel_value_LR1, img_in_Gray_non_bgcolor_LR1
 
 
 
-def determineCorrectParam(_ratio_of_reference_section):
-    print("Ratio of reference section       :", _ratio_of_reference_section*100, "(%)")
-
-    # Determine standard pixel value in the input image(LR=1)
+def determineCorrectParameter(_ratio_of_reference_section):
+    # Initialize
     tmp_reference_section         = 0.0
     standard_pixel_value_LR1      = max_pixel_value_LR1
+
+    # Determine standard pixel value in the input image(LR=1)
     while tmp_reference_section < _ratio_of_reference_section:
         # Temporarily calc    
         sum_pixels_in_section     = np.sum( (standard_pixel_value_LR1 <= img_in_Gray_LR1) )
-        tmp_reference_section     = sum_pixels_in_section / N_all_nonzero_LR1
+        tmp_reference_section     = sum_pixels_in_section / N_all_non_bgcolor_LR1
 
         # Next pixel value
         standard_pixel_value_LR1 -= 1
@@ -275,7 +315,7 @@ def determineCorrectParam(_ratio_of_reference_section):
 
         # Then, calc ratio of max pixel value(LR=1)
         tmp_sum_pixels_in_section = np.sum(standard_pixel_value_LR1 <= tmp_corrected_img_Gray)
-        tmp_ratio = tmp_sum_pixels_in_section / N_all_nonzero
+        tmp_ratio = tmp_sum_pixels_in_section / N_all_non_bgcolor
 
         # Update parameter
         p += p_interval
@@ -292,10 +332,10 @@ def correctPixelValue(_p_final, _standard_pixel_value_LR1):
     img_corrected_Gray = cv2.cvtColor(img_corrected_RGB, cv2.COLOR_RGB2GRAY)
 
     print("p_final                          :", _p_final)
-    print("Ratio of num. of pixels to 255   :", round(np.sum(img_corrected_Gray==255) / N_all_nonzero * 100, 2), "(%)")
+    #print("Ratio of num. of pixels to 255   :", round(np.sum(img_corrected_Gray==255) / N_all_non_bgcolor * 100, 2), "(%)")
 
     # Create figure
-    createHistogram(img_in_RGB_LR1, img_in_RGB, img_corrected_RGB, _standard_pixel_value_LR1)
+    createFigure(img_in_RGB_LR1, img_in_RGB, img_corrected_RGB, _standard_pixel_value_LR1)
 
     return img_corrected_RGB
 
@@ -323,10 +363,10 @@ def saveFigureAndImages(_p_final, _img_in_RGB, _img_corrected_RGB):
 def execCommand(_fig_name, _input_img_name, _corrected_img_name):
     preview_command = ['open', _fig_name, _input_img_name, _corrected_img_name]
     try:
-      res = subprocess.check_call(preview_command)
+        res = subprocess.check_call(preview_command)
 
     except:
-      print("ERROR")
+        print("ERROR")
 
 
 
@@ -335,16 +375,19 @@ if __name__ == "__main__":
     img_in_RGB      = readImage(args[1])
     img_in_RGB_LR1  = readImage(args[2])
 
+    # Check background color
+    bgcolor = checkBackgroundColor(args[3])
+
     print("\n\n====================================")
     print(" STEP1 : Get max pixel value (LR=1)")  
     print("====================================")
-    N_all_nonzero, N_all_nonzero_LR1, img_in_Gray_LR1, max_pixel_value_LR1, ratio_max_pixel_value_LR1 = preProcess()
+    N_all_non_bgcolor = preProcess()
+    N_all_non_bgcolor_LR1, img_in_Gray_LR1, max_pixel_value_LR1, ratio_max_pixel_value_LR1, img_in_Gray_non_bgcolor_LR1 = preProcess4LR1()
 
     print("\n\n================================================")
     print(" STEP2 : Search for standard pixel value (LR=1)")
     print("================================================")
-    ratio_of_reference_section = 0.01 # 1(%)
-    p_final, standard_pixel_value_LR1 = determineCorrectParam(ratio_of_reference_section)
+    p_final, standard_pixel_value_LR1 = determineCorrectParameter(ratio_of_reference_section)
 
     print("\n\n=============================")
     print(" STEP3 : Correct pixel value")
